de9835727f7cb184f0dd777c597a855d9cd77eea
unknown <74397306+Shiyastra@users.noreply.github.com>
2021-03-10 15:22:01 -0800

    Update words.js

--- words.js
+++ words.js
@@ -3,7 +3,8 @@
 
 const prompts = [
     ["hello", "hilo", "hi", "hey", "heya", "hey", "good morning", "good afternoon", "good evening" ],
-    [""]
+    ["how are you", "how is life", "how are things", "how's it going"],
+    ["what are you doing", "what is going on",]
 ]
 
 

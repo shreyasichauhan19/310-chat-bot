d92aeac685c60ee497250d44497902b73fd1eba7
Megan Nguyen <megan.nguyenn261@gmail.com>
2021-03-10 16:53:39 -0800

    Update app.js with read()
    - added read function with some input cleanup (unfinished)
    - reformatted code

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 //event listener for user input
-document.addEventListener("load", function getInput(){//when page is fully loaded, call function getInput
+document.addEventListener("load", function getInput() {//when page is fully loaded, call function getInput
     var userField = document.getElementById("input");//identify text input field
-    userField.addEventListener("keydown", event =>{
-        if(event.keyCode===13){//if enter keydown detected
+    userField.addEventListener("keydown", event => {
+        if (event.keyCode === 13) {//if enter keydown detected
             var userInput = userField.value; //grab user input
             read(userInput);
-            inputField.value=null;//reset the user input field
+            inputField.value = null;//reset the user input field
         }
     })
 })
+
+//cleaning up user input and trying to match it to a prompt in our script with compare(). Then replying with a response with write()
+function read(userInput) {
+    var cleanedInput = userInput;
+
+    //lowercase input
+    cleanedInput = cleanedInput.toLowerCase();
+    //remove unecessary white spaces
+    cleanedInput = cleanedInput.trim();
+    //remove non alphabetical characters
+    cleanedInput = cleanedInput.replace();
+}
 
 function compare(arrayPrompt, arrayReplies, string) {
     let reply;
     let foundReply = false;
     for (let x = 0; x < arrayPrompt.length; x++) {
-      for (let y = 0; y < arrayPrompt[x].length; y++) {
-        if (arrayPrompt[x][y] === string) {
-          let replies = arrayReplies[x];
-          reply = replies[Math.floor(Math.random() * replies.length)];
-          // Inner loop stops when user input value == prompts
-          foundReply = true;
-          
-          break;
+        for (let y = 0; y < arrayPrompt[x].length; y++) {
+            if (arrayPrompt[x][y] === string) {
+                let replies = arrayReplies[x];
+                reply = replies[Math.floor(Math.random() * replies.length)];
+                // Inner loop stops when user input value == prompts
+                foundReply = true;
+
+                break;
+            }
         }
-      }
-      // instead of interating through the entire array stop outer loop when reply is found 
-      if (foundReply) {
-        break;
-      }
+        // instead of interating through the entire array stop outer loop when reply is found 
+        if (foundReply) {
+            break;
+        }
     }
     return reply;
-  }
-  
+}

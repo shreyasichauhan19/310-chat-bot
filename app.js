6a518a5897dc67ee494f932366d8b3b8d8ee3ff2
shreyasichauhan19 <65312818+shreyasichauhan19@users.noreply.github.com>
2021-03-10 16:17:45 -0800

    add compare function to app.js

--- app.js
+++ app.js
@@ -9,3 +9,26 @@ document.addEventListener("load", function getInput(){//when page is fully loade
         }
     })
 })
+
+function compare(arrayPrompt, arrayReplies, string) {
+    let reply;
+    let foundReply = false;
+    for (let x = 0; x < arrayPrompt.length; x++) {
+      for (let y = 0; y < arrayPrompt[x].length; y++) {
+        if (arrayPrompt[x][y] === string) {
+          let replies = arrayReplies[x];
+          reply = replies[Math.floor(Math.random() * replies.length)];
+          // Inner loop stops when user input value == prompts
+          foundReply = true;
+          
+          break;
+        }
+      }
+      // instead of interating through the entire array stop outer loop when reply is found 
+      if (foundReply) {
+        break;
+      }
+    }
+    return reply;
+  }
+  

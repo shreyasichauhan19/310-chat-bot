20a9c6ea3f44ec8dd8ca22f4c8f187c663130c4b
unknown <74397306+Shiyastra@users.noreply.github.com>
2021-03-10 16:06:46 -0800

    Update words.js with replies

--- words.js
+++ words.js
@@ -27,5 +27,23 @@ const prompts = [
 //all possible responses
 
 const replies = [
-    [""]
+    ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy!"],
+    ["Fine... how are you?", "Pretty well, how are you?", "Fantastic, how are you"],
+    ["Nothing much", "About to go to sleep", "Can you guess?", "I don't know actually"],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, Javascript"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Great question"],
+    ["That's ok", "I understand", "What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!", "Good one!"]
 ]

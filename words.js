9310e7cba0aeffaf50b47fbff3af78b3200b54eb
joshmed27 <joshuamed27@gmail.com>
2021-03-12 13:33:35 -0700

    Update Prompts and Replies

--- words.js
+++ words.js
@@ -1,33 +1,11 @@
-//all possible questions
-
-
+/*
+This class stores all the possible word cases within the string and stores a response to the script that the bot will reply to. 
+It uses 2D arrays to store the data that the bot will sort through in order to find the proper case.
+*/ 
 const prompts = [
-    // reference
-    ["hello", "hilo", "hi", "hey", "heya", "hey", "good morning", "good afternoon", "good evening"],
-    ["how are you", "how is life", "how are things", "hows it going"],
-    ["what are you doing", "what is going on", "what is up", "whats up"],
-    ["how old are you"],
-    ["who are you", "are you human", "are you bot", "are you human or bot", "are you bot or human"],
-    ["who created you", "who is your creator", "who made you"],
-    ["your name please", "your name", "may i know your name", "what is your name", "what call yourself", "what are you called"],
-    ["i love you"],
-    ["happy", "good", "fun", "wonderful", "fantastic", "cool", "awesome", "excited"],
-    ["bad", "bored", "tired", "awful", "horrible", "miserable", "angry", "terrible"],
-    ["help me", "tell me story", "tell me joke"],
-    ["ah", "yes", "ok", "okay", "nice", "alright"],
-    ["bye", "good bye", "goodbye", "see you later", "see ya"],
-    ["what should i eat today", "hungry"],
-    ["bro"],
-    ["what", "why", "how", "where", "when", "who"],
-    ["no", "not sure", "maybe", "no thanks"],
-    [""],
-    ["haha", "ha", "lol", "hehe", "heehee", "funny", "joke"],
-    // delete later
-
-
     // beginning of bot script (user replies to bot)
-    [""],
-    ["good", "bad"],
+    [""], //Pretend first case
+    ["good", "bad", "fine", "terrible", "great", "decent", "awful", "ok"],
     [""],
     [""], // user input name
     [""], // user input order number
@@ -52,40 +30,36 @@ const prompts = [
     [""],
     [""], // google form feedback
     [""],
-    [""]
 
-
+    //Default Cases
+    ["hello", "hilo", "hi", "hey", "heya", "hey", "good morning", "good afternoon", "good evening"],
+    ["how are you", "how is life", "how are things", "hows it going"],
+    ["what are you doing", "what is going on", "what is up", "whats up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot", "are you bot or human"],
+    ["who created you", "who is your creator", "who made you"],
+    ["your name please", "your name", "may i know your name", "what is your name", "what call yourself", "what are you called"],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool", "awesome", "excited"],
+    ["bad", "bored", "tired", "awful", "horrible", "miserable", "angry", "terrible"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice", "alright"],
+    ["bye", "good bye", "goodbye", "see you later", "see ya"],
+    ["what should i eat today", "hungry"],
+    ["bro"],
+    ["what", "why", "how", "where", "when", "who"],
+    ["no", "not sure", "maybe", "no thanks"],
+    [""],
+    ["haha", "ha", "lol", "hehe", "heehee", "funny", "joke"]
 ]
 
 
 // all possible responses
 
 const replies = [
-    // reference
-    ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy!"],
-    ["Fine... how are you?", "Pretty well, how are you?", "Fantastic, how are you"],
-    ["Nothing much", "About to go to sleep", "Can you guess?", "I don't know actually"],
-    ["I am infinite"],
-    ["I am just a bot", "I am a bot. What are you?"],
-    ["The one true God, Javascript"],
-    ["I am nameless", "I don't have a name"],
-    ["I love you too", "Me too"],
-    ["Have you ever felt bad?", "Glad to hear it"],
-    ["Why?", "Why? You shouldn't!", "Try watching TV"],
-    ["What about?", "Once upon a time..."],
-    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
-    ["Bye", "Goodbye", "See you later"],
-    ["Sushi", "Pizza"],
-    ["Bro!"],
-    ["Great question"],
-    ["That's ok", "I understand", "What do you want to talk about?"],
-    ["Please say something :("],
-    ["Haha!", "Good one!"],
-    // delete later
-
     
     // beginning of bot script (bot goes first here)
-    ["Hi! Thank you for choosing our store."],
+    ["Hi! Thank you for choosing our store. How are you doing today?"],
     ["How are you doing today?"],
     ["Glad to hear it!"],
     ["Please enter your name"],
@@ -107,19 +81,36 @@ const replies = [
     ["How would you rate this conversation?"],
     ["We're so sorry, would you like to file a complaint?"],
     ["Let us know how we can improve!"],
-    ["Thank you for using our service, we're glad we could be of service!"]
+    ["Thank you for using our service, we're glad we could be of service!"],
+
+    //default replies:
+    ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy!"],
+    ["Fine... how are you?", "Pretty well, how are you?", "Fantastic, how are you"],
+    ["Nothing much", "About to go to sleep", "Can you guess?", "I don't know actually"],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, Javascript"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Great question"],
+    ["That's ok", "I understand", "What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!", "Good one!"],
 
 ]
 
-// randome for any other user input
-
-const alternative = [
-    // reference
-    "Same",
-    "Go on...",
-    "Bro...",
-    "Try again",
-    "I'm listening...",
-    "I don't understand :/"
+//Pulls a random string if user input is completely unknown.
+const defaultResponse = [
+    "My apologies, I dont understand what you are trying to say. Could you rephrase that?",
+    "Please try a different query.",
+    "Please elaborate...",
+    "I don't understand, sorry!",
+    "That does not compute! Please try again."
 ]
-// delete later

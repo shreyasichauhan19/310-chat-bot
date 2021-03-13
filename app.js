6113c567b78433a960d5fceb5ab2fc96cad7d350
Megan Nguyen <megan.nguyenn261@gmail.com>
2021-03-12 16:10:22 -0800

    Fix event handler

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 //event listener for user input
-document.addEventListener("load", function getInput() {//when page is fully loaded, call function getInput
+document.addEventListener("DOMContentLoaded", function getInput() {//when page is fully loaded, call function getInput
     var userField = document.getElementById("input");//identify text input field
     userField.addEventListener("keydown", event => {
-        if (event.keyCode === 13) {//if enter keydown detected
+        if (event.keyCode == 13) {//if enter keydown detected
             var userInput = userField.value; //grab user input
             read(userInput);
-            inputField.value = null;//reset the user input field
+            userField.value = null;//reset the user input field
         }
     })
 })
